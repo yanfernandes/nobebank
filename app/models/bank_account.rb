a16979ede8915fedc8da998b44205f17d9cac309
Yan Fernandes <yanfernandes@gmail.com>
2022-11-14 18:38:50 -0300

    build a home index n login page

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,11 @@
 class BankAccount < ApplicationRecord
   belongs_to :user
+
+  before_validation :load_defaults
+
+  def load_defaults
+    if self.new_record?
+      self.balance = 0.00
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :bank_accounts
   devise_for :users
-  root "home#index"
+  root to: "home#index"
+  get 'home/index'
 end
