da916f08d70cf5dcd283acd8a6ca2832d6393c96
Yan Fernandes <yanfernandes@gmail.com>
2022-11-14 19:22:18 -0300

    build a count to index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def number_of_accounts
+    BankAccount.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :deposits
   resources :bank_accounts
   devise_for :users
+  
   root to: "home#index"
   get 'home/index'
 end
