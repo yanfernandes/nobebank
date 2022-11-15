39dcce3792653a24c0705318464ece0fb7a9f26f
Yan Fernandes <yanfernandes@gmail.com>
2022-11-14 21:23:29 -0300

    build balance views

---
+++ app/controllers/balance_controller.rb
@@ -0,0 +1,5 @@
+class BalanceController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :withdraws
   resources :deposits
   resources :bank_accounts
+  resources :balances
   devise_for :users
   
   root to: "home#index"
